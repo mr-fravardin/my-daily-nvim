53623c8d5b15a7a73d4d1f89dbb28b9e51b6563e
Reed <reed@kubuntu.local>
2023-03-10 23:26:22 +0800

    edit content setup

--- after/plugin/syntax-highlight.lua
+++ after/plugin/syntax-highlight.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "python", "javascript", "html", "css", "lua" },
+  ensure_installed = { "python", "javascript", "html", "css", "lua", "vim", "help", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
